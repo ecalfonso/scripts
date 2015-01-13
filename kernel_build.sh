7cabb2017f9eba3d5f76dbceccae74a603fd646d
Eduard Alfonso <xsynergy510x@gmail.com>
2015-01-13 10:09:55 -0800

    Update ROM build script, create kernel script

--- kernel_build.sh
+++ kernel_build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Set device
-DEVICE=jflte
+DEVICE=jfltetmo
 DATE=$(date +"%Y%m%d-%T")
 
 CLEAN=0
-REMOTE=0
 SYNC=0
 
 for var in "$@"
@@ -13,9 +12,6 @@ do
     case "$var" in
         clean )
             CLEAN=1
-            ;;
-        remote )
-            REMOTE=1
             ;;
         sync )
             SYNC=1
@@ -25,19 +21,28 @@ done
 
 if [[ $SYNC == 1 ]]; then
     echo "Repo sync"
-    repo sync
+    repo sync device/samsung/jf-common
+    repo sync kernel/samsung/jf
 fi
 
 if [[ $CLEAN == 1 ]]; then
     echo "Cleaning build directory"
     rm -rf ~/.ccache
+    rm log*.out
     make clean
     make clobber
 fi
 
 # Setup build environment
 . build/envsetup.sh
+
+# Don't build recovery
 export BUILDING_RECOVERY=false
+
+# Enable -O3 flags
+export USE_O3_OPTIMIZATIONS=true
+
+# Remove old build.prop
 if [ -e out/target/product/$DEVICE/system/build.prop ]; then
     echo "Removing old build.prop"
     rm out/target/product/$DEVICE/system/build.prop
@@ -46,24 +51,18 @@ fi
 # Start build
 START=$(date +%s.%N)
 echo "Starting build for $DEVICE"
-pb --note -t Starting build for $DEVICE @ $DATE
-brunch $DEVICE 2>&1 | tee log-$DATE.out
+pb --note -t Starting Kernel build for $DEVICE @ $DATE
+breakfast $DEVICE
+mka bootimage 2>&1 | tee log-$DATE.out
 END=$(date +%s.%N)
 MIN=$(echo "($END-$START)/60"|bc)
 SEC=$(echo "($END-$START)%60"|bc)
 
 # Pushbullet alert when build finishes
 if [ -e log-$DATE.out ]; then
-    if tail log-$DATE.out | grep -q "Package Complete"; then
-        pb --note -t Package complete for $DEVICE -m Elapsed time: $MIN min $SEC sec
-        if [[ $REMOTE == 1 ]]; then
-            echo "Sending zip to Drive"
-            cp out/target/product/jflte/Op*.zip ~/ext_storage/Drive/ROMs/
-            cd ~/ext_storage/Drive
-            grive
-            cd ~/jflte
-        fi
+    if tail log-$DATE.out | grep -q "Made boot image:"; then
+        pb --note -t Kernel complete for $DEVICE -m Elapsed time: $MIN min $SEC sec
     else
-        pb --note -t Build failed for $DEVICE -m Elapsed time: $MIN min $SEC sec 
+        pb --note -t Kernel build failed for $DEVICE -m Elapsed time: $MIN min $SEC sec 
     fi
 fi
