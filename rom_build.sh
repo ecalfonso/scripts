c3df2fa0d92ef601caf0f9b6b7fc3dcd2080a483
Eduard Alfonso <xsynergy510x@gmail.com>
2015-01-25 18:02:13 -0800

    Build with O3 opti for ROM

--- rom_build.sh
+++ rom_build.sh
@@ -39,7 +39,7 @@ fi
 export BUILDING_RECOVERY=false
 
 # Enable -O3 flags
-export USE_O3_OPTIMIZATIONS=false
+export USE_O3_OPTIMIZATIONS=true
 
 # Remove old build.prop
 if [ -e out/target/product/$DEVICE/system/build.prop ]; then
