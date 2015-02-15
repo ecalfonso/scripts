88de049ab21b6442dcdc3fc7e954bd386c6db57d
Eduard A <xsynergy510x@gmail.com>
2015-02-14 17:50:39 -0800

    Create start up script for when I have to set up my build machine again

---
+++ startup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Start up script to configure building essentials
+#	If I ever lose what's on my machine, I save tons
+#	of time running this rather than trying to remember
+#	what I need to re-install
+
+# Start at ~/
+#cd ~/
+
+# Install packages
+#sudo apt-get -y install \
+#	openssh-server \
+#	git \
+#	tmux \
+#	curl \
+#	openjdk-7-jdk \
+#	vim
+	
+# Make system configs
+#echo "vm.swappiness=80" >> /etc/sysctl.conf 
+
+# Set up Github repos
+REPOS="
+	android_build
+	roomservice
+	scripts"
+	
+mkdir ~/github
+cd ~/github
+for i in $REPOS
+	do
+		git clone https://github.com/xsynergy510x/"$i".git
+	done
