db3874791a635fe0c447c8feb6598043b189dae9
Eduard Alfonso <xsynergy510x@gmail.com>
2015-10-24 20:17:58 -0700

    Add my sepolicy

--- startup.sh
+++ startup.sh
@@ -55,6 +55,7 @@ for repo in \
 	android_external_bluetooth_bluedroid \
 	android_external_chromium_org \
 	android_external_libpng \
+	android_external_sepolicy \
 	android_frameworks_av \
 	android_frameworks_base \
 	android_hardware_qcom_display \
